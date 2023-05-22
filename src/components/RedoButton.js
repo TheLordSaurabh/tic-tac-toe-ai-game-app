c8fa1562d073bb7f2e1a2e8f8c91fb85dc62eaaa
TheLordSaurabh <sgguptatech@gmail.com>
2023-05-23 04:35:29 +0530

    Updated UI

--- src/components/RedoButton.js
+++ src/components/RedoButton.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 import "./RedoButton.css";
 
-export const RedoButton = ({ redo }) => {
+
+
+export const RedoButton = ({redo}) => {
     return (
         <button className="reset-btn" onClick={redo}>Redo</button>
     )
--- src/pages/Developer.js
+++ src/pages/Developer.js
@@ -5,16 +5,9 @@ export const Developer = () => {
   return (
   <div className="developers">
   <h4>
-    This project is made by Saurabh Gupta and Ananya M. <br></br>
+    This project is made by Saurabh Gupta. <br></br><br></br>
   Saurabh is Computer Science and Engineering Undergradute (B. Tech CSE
-    (2020-24) @ IIITDM Kancheepuram) while Ananya M. is Computer Science
-    and Artificial Intelligence Undergradute(B. Tech CS-AI (2021-25)
-    @IIITDM Kancheepuram).
-  </h4>
-  <h4>
-    Both together have created this Project under Dr. Ram Prasad Padhy Sir
-    (Professor in Computer Science Field at IIITDM Kancheepuram) under his
-    Course Artificial Intelligence.
+    (2020-24) at IIITDM Kancheepuram).
   </h4>
   </div>
   );
